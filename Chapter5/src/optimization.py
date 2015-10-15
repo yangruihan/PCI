2ab04e9d18978d100ea6ca10d88bdbe04c30873e
C.y <yangruihan@vip.qq.com>
2015-10-15 21:58:52 +0800

    15_10_15

--- Chapter5/src/optimization.py
+++ Chapter5/src/optimization.py
@@ -84,7 +84,123 @@ def schedule_cost(sol):
     return total_price + total_wait
 
 
+# 随机搜索
+# domain 是一个二元元组，它指定了每个变量的最大值和最小值
+# costf 是成本函数，本例中 即 schedule_cost
+def random_optimize(domain, costf):
+    best = 999999999
+    best_r = None
+    # 随机产生1000次猜测，并对每一次猜测调用成本函数
+    for i in range(1000):
+        # 创建一个随机解
+        r = [random.randint(domain[i][0], domain[i][1])
+                for i in range(len(domain))]
+
+        # 得到成本
+        cost = costf(r)
+
+        # 与到目前为止的最优解进行比较
+        if cost < best:
+            best = cost
+            best_r = r
+    return r
+
+
+# 爬山法
+def hill_climb(domain, costf):
+    # 创建一个随机解
+    sol = [random.randint(domain[i][0], domain[i][1])
+            for i in range(len(domain))]
+
+    # 主循环
+    while 1:
+
+        # 创建相邻解的列表
+        neighbors = []
+
+        for j in range(len(domain)):
+
+            # 在每个方向上相对于原值偏离一点
+            if sol[j] > domain[j][0]:
+                neighbors.append(sol[0:j] + [sol[j] - 1] + sol[j+1:])
+
+            if sol[j] < domain[j][1]:
+                neighbors.append(sol[0:j] + [sol[j] + 1] + sol[j+1:])
+
+        # 在相邻解种寻找最优解
+        current = costf(sol)
+        best = current
+        for j in range(len(neighbors)):
+            cost = costf(neighbors[j])
+            if cost < best:
+                best = cost
+                sol = neighbors[j]
+
+        # 如果没有更好的解，则退出循环
+        if best == current:
+            break
+
+    return sol
+
+
+# 退火法
+def annealing_optimize(domain, costf, T=10000.0, cool=0.95, step=1):
+    # 随机初始化值
+    vec = [float(random.randint(domain[i][0], domain[i][1]))
+            for i in range(len(domain))]
+
+    while T > 0.1:
+        # 选择一个索引值
+        i = random.randint(0, len(domain) - 1)
+
+        # 选择一个改变索引值的方向
+        dir = random.randint(-step, step)
+
+        # 创建一个代表题解的新列表，改变其中一个值
+        vec_b = vec[:]
+        vec_b[i] += dir
+        if vec_b[i] < domain[i][0]:
+            vec_b[i] = domain[i][0]
+        elif vec_b[i] > domain[i][1]:
+            vec_b[i] = domain[i][1]
+
+        # 计算当前成本和新的成本
+        ea = costf(vec)
+        eb = costf(vec_b)
+
+        # 判断是否为更优解，或者时趋向最优解的可能临界解
+        if (eb < ea or random.random() < pow(math.e, -(eb - ea) / T)):
+            vec = vec_b
+
+        # 降低温度
+        T = T * cool
+
+    return vec
+
+
 if __name__ == '__main__':
-    s = [1, 4, 3, 2, 7, 3, 6, 3, 2, 4, 5, 3]
-    print_schedule(s)
+    domain = [(0, 9)] * (len(people) * 2)
+
+    print '----------random_optimize----------'
+
+    t1 = time.time()
+    s = random_optimize(domain, schedule_cost)
+    print time.time() - t1
     print schedule_cost(s)
+    print_schedule(s)
+
+    print '----------hill_climb----------'
+
+    t1 = time.time()
+    s = hill_climb(domain, schedule_cost)
+    print time.time() - t1
+    print schedule_cost(s)
+    print_schedule(s)
+
+    print '----------annealing_optimize----------'
+
+    t1 = time.time()
+    s = annealing_optimize(domain, schedule_cost)
+    print time.time() - t1
+    print schedule_cost(s)
+    print_schedule([int(x) for x in s])
